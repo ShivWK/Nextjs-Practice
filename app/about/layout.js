c755f125b9f22eb8ab7c4d938ab05ec2886afac0
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-10-16 19:34:34 +0530

    Empliment Custom layout

---
+++ app/about/layout.js
@@ -0,0 +1,10 @@
+const AboutLayout = ({ children }) => {
+    return (
+        <section>
+            <h1>This is About layout</h1>
+            { children }
+        </section>
+    )
+}
+
+export default AboutLayout;
